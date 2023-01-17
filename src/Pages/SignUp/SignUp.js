88c863b56b544895e16008bcfc5ad29d3e810a7d
A K M Mahdi <94472254+AKM-Mahdi@users.noreply.github.com>
2023-01-17 15:52:14 +0600

    registration page added usign react-hook-form

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -3,25 +3,48 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
-const Login = () => {
+const SignUp = () => {
   const {
     register,
     formState: { errors },
     handleSubmit,
   } = useForm();
 
-  const handleLogin = (data) => {
+  const handleRegister = (data) => {
     console.log(data);
   };
 
   return (
-    // react hook form
     <div className="lg:h-[90vh] h-[100vh]  flex  justify-center items-center">
       <form
         className="w-[500px] border-2 rounded-md px-5 py-[30px]"
-        onSubmit={handleSubmit(handleLogin)}
+        onSubmit={handleSubmit(handleRegister)}
       >
-        <h2 className="text-center text-2xl font-bold pb-5">User Login</h2>
+        <h2 className="text-center text-2xl font-bold pb-5">
+          User Registration
+        </h2>
+
+        {/* name  */}
+        <label className="label mt-3">
+          <span className="label-text">Name</span>
+        </label>
+        <input
+          className="input input-bordered w-full text-sm"
+          type="text"
+          {...register("name", {
+            required: "name is required",
+            maxLength: {
+              value: 20,
+              message: "name can not be more then 15 character",
+            },
+          })}
+          placeholder="please enter your name"
+        />
+        {errors.name && (
+          <p className="text-sm text-red-700 pt-2" role="alert">
+            {errors.name?.message}
+          </p>
+        )}
 
         {/* email  */}
         <label className="label mt-3">
@@ -60,18 +83,17 @@ const Login = () => {
             {errors.password?.message}
           </p>
         )}
-        <p className="text-sm py-4">Forget password?</p>
 
         <input
-          className="input btn btn-secondary input-bordered w-full "
+          className="input btn btn-secondary input-bordered w-full mt-3"
           type="submit"
-          value="Login"
+          value="Register"
         />
 
         <p className="text-sm text-center pt-4">
-          New to Doctor's Portal?
+          Have an Account?
           <span className="text-primary">
-            <Link to="/signup"> Create a New Account</Link>
+            <Link to="/login"> Go to Login</Link>
           </span>
         </p>
         <div className="divider">OR</div>
@@ -81,4 +103,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default SignUp;
